432d995277d8a6b976002ed4e2ac01a38510f612
Alejandro_MC <alejomiercadrazco@gmail.com>
2025-11-08 17:15:35 -0500

    Add files via upload

---
+++ FarmaSmart.Models/Suppliers vs Products/M_Inventory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+
+namespace FarmaSmart.Models
+{
+    public class M_Inventory
+    {
+        #region Atributos privados
+        private int _inventoryId;
+        private int _productId;
+        private string _warehouse;
+        private decimal _quantity;
+        private DateTime _lastUpdatedAt;
+
+        // Atributos de manejo de la BD
+        private string _mensajeError;
+        private string _valorScalar;
+        private DataTable _dtResultados;
+        #endregion
+
+        #region Atributos públicos
+        public int InventoryId { get => _inventoryId; set => _inventoryId = value; }
+        public int ProductId { get => _productId; set => _productId = value; }
+        public string Warehouse { get => _warehouse; set => _warehouse = value; }
+        public decimal Quantity { get => _quantity; set => _quantity = value; }
+        public DateTime LastUpdatedAt { get => _lastUpdatedAt; set => _lastUpdatedAt = value; }
+        public string MensajeError { get => _mensajeError; set => _mensajeError = value; }
+        public string ValorScalar { get => _valorScalar; set => _valorScalar = value; }
+        public DataTable DtResultados { get => _dtResultados; set => _dtResultados = value; }
+        #endregion
+    }
+}
---
+++ FarmaSmart.Models/Suppliers vs Products/M_ProductCategory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+
+namespace FarmaSmart.Models
+{
+    public class M_ProductCategory
+    {
+        #region Atributos privados
+        private int _categoryId;
+        private string _name;
+        private string _description;
+        private bool _isActive;
+        private DateTime _createdAt;
+
+        // Atributos de manejo de la BD
+        private string _mensajeError;
+        private string _valorScalar;
+        private DataTable _dtResultados;
+        #endregion
+
+        #region Atributos públicos
+        public int CategoryId { get => _categoryId; set => _categoryId = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Description { get => _description; set => _description = value; }
+        public bool IsActive { get => _isActive; set => _isActive = value; }
+        public DateTime CreatedAt { get => _createdAt; set => _createdAt = value; }
+        public string MensajeError { get => _mensajeError; set => _mensajeError = value; }
+        public string ValorScalar { get => _valorScalar; set => _valorScalar = value; }
+        public DataTable DtResultados { get => _dtResultados; set => _dtResultados = value; }
+        #endregion
+    }
+}
